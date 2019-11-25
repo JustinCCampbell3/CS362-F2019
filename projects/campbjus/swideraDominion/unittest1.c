a934bd2e9565262a5b20994ecbdb282d9e89da2d
JustinCCampbell3 <38171122+JustinCCampbell3@users.noreply.github.com>
2019-11-24 19:45:54 -0800

    unit test files

---
+++ projects/campbjus/swideraDominion/unittest1.c
@@ -0,0 +1,50 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+// Baron Function
+int main(int argc, char **argv){
+
+  //checks to make sure the funciton deletes the currentPlayers baron card after the funciton is executed
+
+  //set kingdom cards array
+  int j[10]={remodel, smithy, village, baron,  great_hall, adventurer, council_room, feast, gardens, mine};
+  int i; //for loop variable
+  G.coins=5; //need enough coins to play card
+  struct gameState *G; //initialize gamestate
+  initializeGame(3, j, 3, &G); //create game
+
+  g->hand[0][0]=baron; //gives the current player a baron card
+  g->hand[0][1]=estate; //gives the current player an estate card to discard
+
+  printf("Testing the Baron function\n");
+  baron(&G, 1, 0); //calls baron funciton
+
+  while(G->hand[0][i]!=NULL){
+
+    assert(G->hand[0][i]!=baron); //checks to make sure card is deleted.
+    i++; //iterate to next card
+  }
+
+  printf("Baron card was deleted\n"); //successful test
+
+  assert(G->numBuys==2); //makes sure numBuys was incremented
+
+  while(G->hand[0][i]!=NULL){
+
+    if(G->hand[0][i]==estate){
+      G->hand[0][i]=mine;
+    } //checks to make sure card is deleted.
+    i++; //iterate to next card
+  }
+
+  baron(&G, 1, 0); //calls baron funciton
+  //not sure what will happen here cause this is testing a part of the code that is not written yet
+
+  return 0;
+}
---
+++ projects/campbjus/swideraDominion/unittest3.c
@@ -0,0 +1,31 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+//Mine Function
+int main(int argc, char **argv){
+
+  //set kingdom cards array
+  int j[10]={remodel, smithy, village, baron,  great_hall, adventurer, council_room, feast, gardens, mine};
+  int i;
+  struct gameState *G; //initalize gamestate
+  initializeGame(3, j, 3, &G); //create game
+
+  G->hand[0][0]=copper;
+
+  printf("Testing mine funciton\n", );
+  i=mine(&G, 0, silver, 0); //calls mine function
+
+  assert(G->hand[0][0]!=copper); //asserts card was discarded
+  assert(G->hand[0][0]==silver); //asserts card was added
+  assert(i!=-1); //asserts function did not incorrectly trigger one of the first three if statements
+  assert(G->handCount==5); //asserts handcount did not change
+
+  printf("Funciton Passed\n", );
+  return 0;
+}
